ec0daa34af42fdeb7fba52b95d11fd76e84057e0
erik.karapetyan <erik.karapetyan@vkteam.ru>
2025-05-28 17:58:36 +0300

    Add third case

--- src/main/java/com/iverpa/mpi/service/WaitingRoomService.java
+++ src/main/java/com/iverpa/mpi/service/WaitingRoomService.java
@@ -1,7 +1,14 @@
 package com.iverpa.mpi.service;
 
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
+
 import com.iverpa.mpi.controller.dto.responses.QueueViewResponse;
+import com.iverpa.mpi.model.Role;
 import com.iverpa.mpi.model.User;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +25,14 @@ public class WaitingRoomService {
 
     private final ConcurrentLinkedQueue<User> queue = new ConcurrentLinkedQueue<>();
 
+    @PostConstruct
+    public void init() {
+        queue.add(new User(
+           3L,
+           "test",
+           "test",
+                Set.of(Role.RECRUIT)
+        ));
     public void send(User user) {
         if (!queue.contains(user)) {
             queue.add(user);
